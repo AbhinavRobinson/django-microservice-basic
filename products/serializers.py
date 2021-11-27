151ca8a55f4059bd1a9bb50dc49040d1890e0486
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-11-27 16:35:41 +0530

    feat(api): create routes for crud ops

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        modal = Product
+        model = Product
         fields = '__all__'
--- products/views.py
+++ products/views.py
@@ -1,8 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 class ProductViewSet(viewsets.ViewSet):
-    def list(self, request):
-        pass
 
-    def create(self, request):
-        pass
+    # /api/products
+    def list(self, request: Request) -> Response:
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request: Request) -> Response:
+        print(request.data)
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):  # <str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):  # <str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):  # <str:id>
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
